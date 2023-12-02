a5f017c01a174d02f24bd9eae0a3222fc2648635
Sten Lankreijer <stenlan@Stens-MacBook-Pro.local>
2023-12-02 10:28:17 +0100

    Day 2

---
+++ src/2.ts
@@ -0,0 +1,51 @@
+import { readFile } from "fs/promises";
+
+const fileLines = (await readFile("inputs/input2.txt")).toString().split("\n");
+
+type CubeColor = "red"|"green"|"blue";
+type BagContents = Record<CubeColor, number>;
+
+const contents0: BagContents = {
+    red: 12,
+    green: 13,
+    blue: 14
+}
+
+let sumPossible = 0;
+let powerSum = 0;
+
+for (const line of fileLines) {
+    if (line.length === 0) continue;
+
+    const minContents: BagContents = {
+        red: 0,
+        green: 0,
+        blue: 0
+    }
+    const [gameNumStr, gameDataStr] = line.split(": ");
+    const gameNum = parseInt(gameNumStr.substring(5));
+
+    const grabStrs = gameDataStr.split("; ");
+
+    let possible = true;
+
+    for (const grabStr of grabStrs) {
+        for (const singleGrabStr of grabStr.split(", ")) {
+            const [grabNumStr, grabColor] = singleGrabStr.split(" ") as [string, CubeColor];
+
+            const grabNum = parseInt(grabNumStr);
+
+            if (grabNum > contents0[grabColor]) {
+                possible = false;
+            }
+
+            minContents[grabColor] = Math.max(minContents[grabColor], grabNum);
+        }
+    }
+
+    if (possible) sumPossible += gameNum;
+    powerSum += minContents["red"] * minContents["green"] * minContents["blue"];
+}
+
+console.log(sumPossible);
+console.log(powerSum);
